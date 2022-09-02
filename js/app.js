dd1bebb1e6726414e59cdffcbaa423df07dab22f
Rubayet-billah <rubayetbillah909@gmail.com>
2022-09-03 01:21:12 +0600

    onclick function added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('connected')
+const loadNews = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    newsCategoris(data.data.news_category)
+}
+const newsCategoryContainer = document.getElementById('news-title');
+const newsCategoris = (categories) => {
+    // console.log(categories)
+    for(const category of categories){
+        // console.log(category.category_id);
+
+        const li = document.createElement('li');
+        li.classList.add('m-2');
+        li.classList.add('target-category');
+        li.innerHTML = `
+            <p onclick= "getCategoryId('${category.category_id}')">${category.category_name}</p>   
+        `;
+        newsCategoryContainer.appendChild(li);
+    }
+}
+
+
+
+loadNews();
+
