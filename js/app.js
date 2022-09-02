bef6b06f213f6607935d79d7ba64451a3784d23e
Rubayet-billah <rubayetbillah909@gmail.com>
2022-09-03 01:53:16 +0600

    display newses

--- js/app.js
+++ js/app.js
@@ -36,15 +36,32 @@ const categoryIdNews = async(id) => {
 
 // display news by category id
 const newsCountSection = document.getElementById('news-count-section');
+const newsContainerSection = document.getElementById('news-container');
 const displayNews = async(news) => {
     console.log(news);
     if(news.length !== 0){
         newsCountSection.innerText = `You have ${news.length} news to read in this category`;
     }
     else{
-        newsCountSection.innerText = `
-           You have no news to read in this category
-        `;
+        newsCountSection.innerText = `You have no news to read in this category`;
     }
+
+    // display news in container
+    newsContainerSection.innerHTML =`
+    <div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4 col-sm-12">
+        <img src="..." class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
 
 }
